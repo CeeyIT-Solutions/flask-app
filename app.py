7122f57f32fe99e0f9f4f27dbcb146c99827b762
ceeyIT <104657912+ceeyIT@users.noreply.github.com>
2023-02-07 06:16:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #!/bin/python
+import logging
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,4 +9,7 @@ def index():
     return "I am almost a Devops Engineer!"
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.info('Starting Flask app...')
     app.run(host='0.0.0.0')
+    logging.info('Flask app has been started')
